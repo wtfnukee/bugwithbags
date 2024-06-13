6bcc3dca767e5adf504fb647d9af22606b97c1c0
wtfnukee <hehenukee@gmail.com>
2024-06-13 11:23:07 +0000

    latest stable

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -3,8 +3,8 @@ use hyper::{Body, Response};
 use log::info;
 
 pub async fn handle_index() -> Result<Response<Body>, hyper::Error> {
-    info!("/index hitted");
-    let mut response = Response::new(Body::from("Hello World"));
+    info!("/index hit");
+    let mut response = Response::new(Body::from("Bug with Bags"));
     response.headers_mut().insert(
         CONTENT_TYPE,
         HeaderValue::from_static("text/plain; charset=utf-8"),
